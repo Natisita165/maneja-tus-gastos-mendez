6c322fbb8533cf41a2a14a68a099aef69c046ef3
Natisita165 <natisita6@gmail.com>
2026-03-10 21:10:50 -0400

    Changes 2

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: true,
   // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
+  // New comment
   apiUrl: 'https://maneja-tus-gastos-api-bq3s.onrender.com',
 };
+
+
 
